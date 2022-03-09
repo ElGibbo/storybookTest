236dc5f076f752966954fec7b5447d671a8ead08
Steve Gibbins <steve.gibbins@responsetap.com>
2022-03-09 13:30:50 +0000

    moar updates

--- src/lib/store.js
+++ src/lib/store.js
@@ -1,7 +1,7 @@
 /* A simple redux store/actions/reducer implementation.
  * A true app would be more complex and separated into different files.
  */
-import { configureStore, createSlice } from '@reduxjs/toolkit';
+import { configureStore, createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 
 /*
  * The initial state of our store when the app loads.
@@ -14,10 +14,23 @@ const defaultTasks = [
   { id: '4', title: 'Something again', state: 'TASK_INBOX' },
 ];
 const TaskBoxData = {
-  tasks: defaultTasks,
-  status: 'idle',
-  error: null,
+    tasks: defaultTasks,
+    status: 'idle',
+    error: null,
 };
+
+export const fetchTasks = createAsyncThunk('todos/fetchTodos', async () => {
+    const response = await fetch(
+        'https://jsonplaceholder.typicode.com/todos?userId=1'
+    );
+    const data = await response.json();
+    const result = data.map((task) => ({
+        id: `${task.id}`,
+        title: task.title,
+        state: task.completed ? 'TASK_ARCHIVED' : 'TASK_INBOX',
+    }));
+    return result;
+});
 
 /*
  * The store is created here.
@@ -25,18 +38,42 @@ const TaskBoxData = {
  * https://redux-toolkit.js.org/api/createSlice
  */
 const TasksSlice = createSlice({
-  name: 'taskbox',
-  initialState: TaskBoxData,
-  reducers: {
-    updateTaskState: (state, action) => {
-      const { id, newTaskState } = action.payload;
-      const task = state.tasks.findIndex((task) => task.id === id);
-      if (task >= 0) {
-        state.tasks[task].state = newTaskState;
-      }
+    name: 'taskbox',
+    initialState: TaskBoxData,
+    reducers: {
+        updateTaskState: (state, action) => {
+            const { id, newTaskState } = action.payload;
+            const task = state.tasks.findIndex((task) => task.id === id);
+            if (task >= 0) {
+                state.tasks[task].state = newTaskState;
+            }
+        },
     },
-  },
+    /*
+    * Extends the reducer for the async actions
+    * You can read more about it at https://redux-toolkit.js.org/api/createAsyncThunk
+    */
+    extraReducers(builder) {
+        builder
+            .addCase(fetchTasks.pending, (state) => {
+                state.status = 'loading';
+                state.error = null;
+                state.tasks = [];
+            })
+            .addCase(fetchTasks.fulfilled, (state, action) => {
+                state.status = 'succeeded';
+                state.error = null;
+                // Add any fetched tasks to the array
+                state.tasks = action.payload;
+            })
+            .addCase(fetchTasks.rejected, (state) => {
+                state.status = 'failed';
+                state.error = "Something went wrong";
+                state.tasks = [];
+            });
+    },
 });
+
 
 // The actions contained in the slice are exported for usage in our components
 export const { updateTaskState } = TasksSlice.actions;
@@ -47,9 +84,9 @@ export const { updateTaskState } = TasksSlice.actions;
  * https://redux-toolkit.js.org/api/configureStore
  */
 const store = configureStore({
-  reducer: {
-    taskbox: TasksSlice.reducer,
-  },
+    reducer: {
+        taskbox: TasksSlice.reducer,
+    },
 });
 
 export default store;
